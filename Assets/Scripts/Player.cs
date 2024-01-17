c61eea16687ffbc36d5c43e548c480ba0a8501fb
Furkan Yılmaz <furkanyilmaz870@gmail.com>
2024-01-17 15:17:29 +0300

    feat: Created all attack animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,41 +2,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D _rb;
-    private Animator _anim;
-    private int _facingDir = 1;
-    private bool _isFacingRight = true;
-    
+    [Header("Movement Variables")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
-    
+    private int _facingDir = 1;
+    private bool _isFacingRight = true;
     private float _xInput;
 
-    [Header("Dash Info")] 
+    [Header("Dash Info")]
     [SerializeField] private float dashDuration;
     [SerializeField] private float dashSpeed;
-    [SerializeField] private float dashTime;
-    
+    [SerializeField] private float dashCooldown;
+    private float _dashTime;
+    private float _dashCooldownTimer;
+
     [Header("Collision Info")]
-    [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
     private bool _isGrounded;
 
+    [Header("Attack Info")] 
+    private bool _isAttacking;
+    private int _comboCounter;
+    
+    private Animator _anim;
+    private Rigidbody2D _rb;
+    
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponentInChildren<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
         CollisionChecks();
         Movement();
         CheckInput();
         FlipController();
         AnimatorControllers();
-        
-        dashTime-= Time.deltaTime;
+
+        _dashCooldownTimer -= Time.deltaTime;
+        _dashTime -= Time.deltaTime;
     }
 
     private void CheckInput()
@@ -46,13 +53,21 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
             Jump();
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            dashTime = dashDuration;
+        if (Input.GetKeyDown(KeyCode.LeftShift)) 
+            DashAbility();
+    }
+
+    private void DashAbility()
+    {
+        if (_dashCooldownTimer > 0) return;
+
+        _dashTime = dashDuration;
+        _dashCooldownTimer = dashCooldown;
     }
 
     private void Movement()
     {
-        if (dashTime > 0)
+        if (_dashTime > 0)
             _rb.velocity = new Vector2(_xInput * dashSpeed, _rb.velocity.y);
         else
             _rb.velocity = new Vector2(_xInput * moveSpeed, _rb.velocity.y);
@@ -65,7 +80,7 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
-        if(_isGrounded)
+        if (_isGrounded)
             _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
     }
 
@@ -82,12 +97,20 @@ public class Player : MonoBehaviour
             Flip();
         else if (_rb.velocity.x < 0 && _isFacingRight)
             Flip();
+        
+        // switch (_rb.velocity.x)
+        // {
+        //     case > 0 when !_isFacingRight:
+        //     case < 0 when _isFacingRight:
+        //         Flip();
+        //         break;
+        // }
     }
 
     private void AnimatorControllers()
     {
         bool isMoving = _rb.velocity.x != 0;
-        
+
         _anim.SetFloat("yVelocity", _rb.velocity.y);
         _anim.SetBool("isMoving", isMoving);
         _anim.SetBool("isGrounded", _isGrounded);
